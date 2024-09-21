c71a315ab86931bb0ca60bb2b0c9bfa68f20bef5
nightness <joshguyette@yahoo.com>
2024-09-21 10:11:40 -0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    // webpack: (config, { isServer }) => {
+    //     if (!isServer) {
+    //         config.resolve.alias['@sentry/node'] = '@sentry/browser';
+    //     }
+
+    //     // PostCSS loader
+    //     const postcssLoader = config.module.rules.find((rule) => rule.test.toString().includes('css'));
+    //     postcssLoader.use.push({
+    //         loader: 'postcss-loader',
+    //         options: {
+    //             postcssOptions: {
+    //                 plugins: {
+    //                     tailwindcss: {},
+    //                 },
+    //             },
+    //         },
+    //     });
+
+    //     return config;
+    // }
 };
 
 export default nextConfig;
--- src/config/config.env.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SERVER_URL = exports.SERVER_PORT = void 0;
-exports.SERVER_PORT = process.env.SERVER_PORT || 3000;
-exports.SERVER_URL = process.env.SERVER_URL || 'http://localhost:3000';
