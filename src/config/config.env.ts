379e3928d8dce6f817f9028fae8c6828788fde09
nightness <joshguyette@yahoo.com>
2024-09-26 02:59:16 -0500

    updates

--- src/config/config.env.ts
+++ src/config/config.env.ts
@@ -1,2 +1,4 @@
-export const SERVER_PORT = process.env.SERVER_PORT || 3000;
+export const SERVER_PORT = process.env.SERVER_PORT || "3000";
 export const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3000';
+export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const HYBRID_ENV = process.env.HYBRID_ENV || true;
--- src/main.ts
+++ src/main.ts
@@ -10,27 +10,31 @@ import configApp from './config/config.app';
 import configSecurityPolicy from './config/config.security';
 import configSwagger from './config/config.swagger';
 import * as nextConfig from '../next.config.js';
+import { HYBRID_ENV, NODE_ENV } from './config/config.env';
 
 NestFactory.create<NestExpressApplication>(AppModule).then(async (app) => {
-  // Setup Next.js
-  const dev = process.env.NODE_ENV !== 'production';
-  const nextApp = Next({ dev, conf: nextConfig, dir: './' });
-  const handle = nextApp.getRequestHandler();
-  await nextApp.prepare();
+  // Enable the hybrid environment and run Next.js as middleware (defaults to true)
+  if (HYBRID_ENV === 'development') {
+    const dev = NODE_ENV !== 'production';
+    const nextApp = Next({ dev, conf: nextConfig, dir: './' });
+    const handle = nextApp.getRequestHandler();
+    await nextApp.prepare();
 
-  // Set global prefix for API routes
-  app.setGlobalPrefix('api');
+    // Set global prefix for API routes
+    app.setGlobalPrefix('api');
+
+    // Middleware to handle Next.js routing
+    app.use((req: Request, res: Response, next: NextFunction) => {
+      if (req.url.startsWith('/api') || req.url.startsWith('/swagger') || req.url.startsWith('/css')) {
+        return next();
+      }
+      return handle(req, res);
+    });
+  }
 
   // Config Security Policy
   configSecurityPolicy(app);
 
-  // Middleware to handle Next.js routing
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    if (req.url.startsWith('/api') || req.url.startsWith('/swagger') || req.url.startsWith('/css')) {
-      return next();
-    }
-    return handle(req, res);
-  });
 
   // Config App
   const listen = configApp(app);
