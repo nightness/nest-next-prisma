475316309d9e76e3cf426cd02ccfd1d3ec3c608f
nightness <joshguyette@yahoo.com>
2024-09-27 06:45:13 -0500

    refactored

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@
 
 // app/page.tsx
 import React from 'react';
-import Dashboard from './_features/dashboard/Dashboard';
+import Dashboard from './_features/mui-free-examples/dashboard/Dashboard';
 
 export default async function HomePage() {
   return (
