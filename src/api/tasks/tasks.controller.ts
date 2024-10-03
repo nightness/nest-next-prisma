084643cde3ccf434d953f794e756d95a5c3fe7bc
nightness <joshguyette@yahoo.com>
2024-10-03 10:18:13 -0500

    added swagger decorator

--- src/api/tasks/tasks.controller.ts
+++ src/api/tasks/tasks.controller.ts
@@ -11,8 +11,10 @@ import {
   } from '@nestjs/common';
   import { TasksService } from './tasks.service';
   import { Task } from '@prisma/client';
+import { ApiTags } from '@nestjs/swagger';
   
   @Controller('tasks')
+  @ApiTags('Tasks')
   export class TasksController {
     constructor(private readonly tasksService: TasksService) {}
   
