e507d63dee0656050de92dd9085a11968f913cc7
nightness <joshguyette@yahoo.com>
2024-10-04 01:14:35 -0500

    added serverFetch

---
+++ utils/serverFetch.ts
@@ -0,0 +1,37 @@
+import { getBaseUrl } from "./getBaseUrl";
+
+export async function serverFetch<T>(url: string, options: RequestInit): Promise<[number, T | null]> {
+  const baseUrl = getBaseUrl();
+  const response = await fetch(`${baseUrl}${url}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      ...options.headers,
+    },
+    ...options,
+  });
+
+
+  // if (!res.ok) {
+  //   const error = await res.text();
+  //   throw new Error(error);
+  // }
+
+  if (!response.ok) {
+    let error = 'Request failed';
+    try {
+      error = await response.text();
+    } catch (e) {}
+    throw new Error(error);
+  }
+
+  // Check if response is empty
+  if (response.status !== 204) {
+    // Check that content type is JSON
+    if (!response.headers.get('content-type')?.includes('application/json')) {
+      throw new Error('Invalid content type');
+    }
+    return [response.status, await response.json() as T];
+  }
+
+  return [response.status, null];
+}
