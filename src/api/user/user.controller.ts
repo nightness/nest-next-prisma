d07fa3c731a46253e7453956c0dfa1a3fc510107
nightness <joshguyette@yahoo.com>
2024-10-05 11:37:15 -0500

    added user controller

---
+++ src/api/user/user.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Req } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { UserService } from './user.service';
+import type { Request } from '@/src/system/types';
+import { JwtPayload } from '../auth/auth.types';
+
+@Controller('user')
+@ApiTags('User')
+export class UserController {
+  constructor(
+    private userService: UserService,
+  ) { }
+
+  @Get('me')
+  @ApiOperation({ summary: 'Get the current user' })
+  meRequest(@Req() req: Request) {    
+    const user = req.user;
+    if (!user) {
+      return null;
+    }
+    return this.userService.findById(user.sub);
+  }
+}
