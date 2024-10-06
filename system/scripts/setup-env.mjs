b9b056f50a63de4c813c6136613b0687858969c2
nightness <joshguyette@yahoo.com>
2024-10-06 10:23:31 -0500

    bug fixes

---
+++ system/scripts/setup-env.mjs
@@ -0,0 +1,32 @@
+import isDocker from 'is-docker';
+import { constants, copyFileSync, existsSync } from 'fs';
+import path from 'path';
+
+function main() {
+  // Don't run in Docker
+  if (isDocker()) {
+    return;
+  }
+
+  const envPath = path.resolve('.env');
+  const envDockerPath = path.resolve('.env.docker');
+  const envDockerDevPath = path.resolve('.env.docker.dev');
+
+  const dotenvInitPath = path.resolve('system/dotenv-init');
+
+  // Pass this flag to skip copying the .env.docker and .env.docker.dev files
+  const noDocker = process.argv.includes('--no-docker');
+
+  if (!existsSync(envPath)) {
+    copyFileSync(`${dotenvInitPath}/.env`, './.env', constants.COPYFILE_EXCL);
+  }
+
+  if (!noDocker && !existsSync(envDockerPath)) {
+    copyFileSync(`${dotenvInitPath}/.env.docker`, './.env.docker', constants.COPYFILE_EXCL);
+  }
+
+  if (!noDocker && !existsSync(envDockerDevPath)) {
+    copyFileSync(`${dotenvInitPath}/.env.docker.dev`, './.env.docker.dev', constants.COPYFILE_EXCL);
+  }
+}
+main();
