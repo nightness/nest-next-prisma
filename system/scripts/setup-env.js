be28ff2dcd66bb4c68542553b96b758c74e5ca4f
nightness <joshguyette@yahoo.com>
2024-10-06 09:46:49 -0500

    testing

--- system/scripts/setup-env.js
+++ system/scripts/setup-env.js
@@ -1,10 +1,10 @@
-const { copyFileSync, constants } = require('fs');
+const { constants, copyFileSync, existsSync } = require('fs');
 const path = require('path');
 
 const envPath = path.resolve('.env');
 const dotenvInitPath = path.resolve('system/dotenv-init');
 
-if (!fs.existsSync(envPath)) {
+if (!existsSync(envPath)) {
   copyFileSync(`${dotenvInitPath}/.env`, './.env', constants.COPYFILE_EXCL);
   copyFileSync(`${dotenvInitPath}/.env.docker`, './.env.docker', constants.COPYFILE_EXCL);
   copyFileSync(`${dotenvInitPath}/.env.docker.dev`, './.env.docker.dev', constants.COPYFILE_EXCL);
