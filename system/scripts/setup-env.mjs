d20f39bbf420c1d9a22b0c49a4eb137e0ed5c425
nightness <joshguyette@yahoo.com>
2024-10-06 18:54:49 -0500

    comments

--- system/scripts/setup-env.mjs
+++ system/scripts/setup-env.mjs
@@ -3,29 +3,33 @@ import isDocker from 'is-docker';
 import { constants, copyFileSync, existsSync } from 'fs';
 import path from 'path';
 
+// This script is run after running `npm install`
+// Currently, it copies the .env files to the root of the project if they don't exist
+
 function main() {
-  // Don't run in Docker
+  // These files are not copied to the container (so they wont exist in the container when running install)
+  // Docker will handle its environment internally from the .env.docker files
   if (isDocker()) {
     return;
   }
 
+  // Paths to the .env files
   const envPath = path.resolve('.env');
   const envDockerPath = path.resolve('.env.docker');
   const envDockerDevPath = path.resolve('.env.docker.dev');
 
+  // If there is no Dockerfile, we don't need the .env.docker files
+  const dockerfilePath = path.resolve('Dockerfile');
+  const noDocker = !existsSync(dockerfilePath);
+
+  // Copy the .env files if they don't exist
   const dotenvInitPath = path.resolve('system/dotenv-init');
-
-  // Pass this flag to skip copying the .env.docker and .env.docker.dev files
-  const noDocker = process.argv.includes('--no-docker');
-
   if (!existsSync(envPath)) {
     copyFileSync(`${dotenvInitPath}/.env`, './.env', constants.COPYFILE_EXCL);
   }
-
   if (!noDocker && !existsSync(envDockerPath)) {
     copyFileSync(`${dotenvInitPath}/.env.docker`, './.env.docker', constants.COPYFILE_EXCL);
   }
-
   if (!noDocker && !existsSync(envDockerDevPath)) {
     copyFileSync(`${dotenvInitPath}/.env.docker.dev`, './.env.docker.dev', constants.COPYFILE_EXCL);
   }
