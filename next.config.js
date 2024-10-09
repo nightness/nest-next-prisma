23040b24d810bad5e39b58b563ca8da4e92d7ee0
nightness <joshguyette@yahoo.com>
2024-10-09 06:18:38 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 // next.config.js
-const withPWA = require('@ducanh2912/next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   // disable: process.env.NODE_ENV !== 'production',
   runtimeCaching: [
