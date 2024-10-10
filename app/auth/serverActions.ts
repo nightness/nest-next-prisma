e0199e36b9b1930fbca60b3ab7a6be50e4fb5a12
nightness <joshguyette@yahoo.com>
2024-10-10 15:33:02 -0500

    start of auth/serverActions.ts

--- app/auth/serverActions.ts
+++ app/auth/serverActions.ts
@@ -3,6 +3,7 @@
 import "server-only";
 
 // import { revalidatePath } from 'next/cache';
+import * as bcrypt from 'bcryptjs';
 import { prisma } from 'lib/prisma';
 import redisUtils from "@/lib/redisUtils";
 
@@ -16,6 +17,23 @@ export async function verifyEmail(token: string) {
   await prisma.user.update({
     where: { email },
     data: { isEmailVerified: true },
+  });
+
+  await redisUtils.del(token);
+}
+
+// password reset via token
+export async function resetPassword(token: string, newPassword: string) {
+  const email = await redisUtils.get(token);
+  if (!email) {
+    throw new Error('Invalid token');
+  }
+
+  // Update hashed password
+  const password = await bcrypt.hash(newPassword, 10);
+  await prisma.user.update({
+    where: { email },
+    data: { password },
   });
 
   await redisUtils.del(token);
