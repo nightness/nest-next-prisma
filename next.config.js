dd53acd627e53671ecb1105f45bc6b692a69f0de
nightness <joshguyette@yahoo.com>
2024-10-10 14:44:08 -0500

    replaced missing disable

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
-  // disable: process.env.NODE_ENV !== 'production',
+  disable: process.env.NODE_ENV !== 'production',
   runtimeCaching: [
     {
       urlPattern: /^\/api\/.*$/,
