560d25a79a5b36bb92aac724b212fba6454638ff
nightness <joshguyette@yahoo.com>
2024-10-11 02:05:57 -0500

    fix

--- app/auth/verify-email/complete/page.tsx
+++ app/auth/verify-email/complete/page.tsx
@@ -1,6 +1,7 @@
 // auth/verify-email/complete/page.tsx
 import styles from '../../(styles)/auth.module.css';
 import Link from 'next/link';
+import { verifyEmail } from '../../serverActions';
 
 interface Props {
   searchParams: { token?: string };
@@ -13,14 +14,26 @@ export default async function VerifyEmailPage({ searchParams }: Props) {
     throw new Error('Invalid or missing token.');
   }
 
-  return (
-    <div className={styles['form-container']} style={{ textAlign: 'center', maxWidth: '500px' }}>
-      <h1 className={styles['form-title']}>Email Verified!</h1>
-      <p className={styles.message}>
-        Thank you for verifying your email address and for being a part of our community.</p>
-      <Link href="/auth/sign-in" className={styles.button}>
-        Sign In
-      </Link>
-    </div>
-  );
+  try {
+    await verifyEmail(token);
+    return (
+      <div className={styles['form-container']} style={{ textAlign: 'center', maxWidth: '500px' }}>
+        <h1 className={styles['form-title']}>Email Verified!</h1>
+        <p className={styles.message}>
+          Thank you for verifying your email address and for being a part of our community.</p>
+        <Link href="/auth/sign-in" className={styles.button}>
+          Sign In
+        </Link>
+      </div>
+    );
+  } catch (error) {
+    return (
+      <div className={styles['form-container']} style={{ textAlign: 'center', maxWidth: '500px' }}>
+        <h1 className={styles['form-title']}>Email Verification Failed</h1>
+        <p className={`${styles.message} ${styles.error}`}>
+          {(error as Error)?.message || 'An error occurred.'}
+        </p>
+      </div>
+    );
+  }
 }
